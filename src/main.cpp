bd1917f1a2b92410753b588c7c94dd5dd8b6b502
pedrolima <pedro.lima02@ufu.br>
2026-04-22 11:31:33 -0300

    first communit

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
-#include <Arduino.h>
+#include "iikit.h"
 
 void setup()
 {
-
+    Serial.begin(9600);
+    pinMode(def_pin_D1,OUTPUT);
+    delay(2000); // espera USB reconectar após reset
+    Serial.println ("=== ESP32-S3 Teste Minimo ===");
 }
 
 void loop()
 {
+    // APAGADO
+    digitalWrite (def_pin_D1,LOW);
+    Serial.println ("APAGADO");
+    delay (500);
 
+    // ACESSO
+    digitalWrite (def_pin_D1,HIGH);
+    Serial.println ("ACESSO");
+    delay(500);
 }
